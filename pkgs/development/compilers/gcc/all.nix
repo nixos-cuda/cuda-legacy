8b8437829955f0c09728878f78f934356862f376
Artturin <Artturin@artturin.com>
2024-10-17 19:08:19 +0300

    splice.nix: make `pkgs` `splicedPackages` when required
    This will make `pkgs` used in `callPackage`, and `pkgsCross.X.pkgs` have
    packages with `__spliced`.
    
    https://www.github.com/NixOS/nixpkgs/blob/3029741718f4c765fbc5ebf76bea3d6c8ff15fe5/pkgs/development/interpreters/python/passthrufun.nix#L37
    
    https://www.github.com/NixOS/nixpkgs/blob/d2bd9a39dec88eddd5c192abee69939e67f43d12/pkgs/top-level/python-packages.nix#L10720
    
    ```
    nix-repl>
    pkgsCross.aarch64-multiplatform.python3Packages.protobuf4.protobuf.__spliced
    error:
           … while evaluating the attribute
    'aarch64-multiplatform.python3Packages.protobuf4.protobuf.__spliced'
             at
    /home/artturin/nixgits/my-nixpkgs/.worktree/1/pkgs/development/python-modules/protobuf/4.nix:119:13:
              118|   passthru = {
              119|     inherit protobuf;
                 |             ^
              120|   };
    
           error: attribute '__spliced' missing
           at «string»:1:1:
                1|
    pkgsCross.aarch64-multiplatform.python3Packages.protobuf4.protobuf.__spliced
                 | ^
    ```
    
    to
    
    ```
    nix-repl>
    pkgsCross.aarch64-multiplatform.python3Packages.protobuf4.protobuf.__spliced
    {
      buildBuild = «derivation
    /nix/store/s7da5mfvx4h1n86j78knaj9cprglxqz6-protobuf-25.4.drv»;
      buildHost = «derivation
    /nix/store/s7da5mfvx4h1n86j78knaj9cprglxqz6-protobuf-25.4.drv»;
      buildTarget = «repeated»;
      hostHost = «derivation
    /nix/store/mszvybzs4zxh43awyrjnybsfcb265n9r-protobuf-aarch64-unknown-linux-gnu-25.4.drv»;
      hostTarget = «repeated»;
    }
    ```
    
    Co-authored-by: Wolfgang Walther <walther@technowledgy.de>

--- pkgs/development/compilers/gcc/all.nix
+++ pkgs/development/compilers/gcc/all.nix
@@ -2,6 +2,8 @@
   lib,
   stdenv,
   pkgs,
+  overrideCC,
+  buildPackages,
   targetPackages,
   callPackage,
   isl_0_20,
@@ -60,7 +62,7 @@ let
                 )
                 && stdenv.cc.isGNU
               then
-                pkgs."gcc${majorVersion}Stdenv"
+                overrideCC stdenv buildPackages."gcc${majorVersion}"
               else
                 stdenv;
           }
