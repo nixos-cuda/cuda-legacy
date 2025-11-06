4e4c00bc20058224467f2f0aa014136544abf0b1
Connor Baker <ConnorBaker01@gmail.com>
2025-11-05 20:10:32 -0800

    overlays/auto.nix: work around aliases marked as throws
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- overlays/auto.nix
+++ overlays/auto.nix
@@ -2,13 +2,25 @@
 # gccVersions, gccStdenvVersions, and cudaPackagesVersions.
 final: prev:
 let
+  inherit (builtins)
+    hasAttr
+    mapAttrs
+    seq
+    tryEval
+    ;
+
   getAttrOr =
     attrs: name: value:
-    attrs.${name} or value;
+    # Attributes may have been aliases since removed and replaced with throws;
+    # can't just check for existence, need to catch errors as well.
+    # NOTE: .value is false if eval fails, else it is the result of evaluation.
+    # NOTE: We must use seq to force accessing the value, ensuring it is not a throw.
+    # NOTE: We cannot use deepSeq as the value may be recursive.
+    if (tryEval (seq (attrs.${name} or null) (hasAttr name attrs))).value then attrs.${name} else value;
 
   # If the attribute exists in `prev` already, pass it through unchanged.
   # Otherwise, use the value provided.
-  unchangedIfPresent = builtins.mapAttrs (getAttrOr prev);
+  unchangedIfPresent = mapAttrs (getAttrOr prev);
 in
 unchangedIfPresent prev.gccVersions
 // unchangedIfPresent prev.gccStdenvVersions
@@ -16,5 +28,5 @@ unchangedIfPresent prev.gccVersions
 // {
   # TODO(@connorbaker): Find a better way to do this, perhaps via groupBy, sorting versions,
   # and choosing the latest for a major release if there isn't one.
-  cudaPackages_11 = prev.cudaPackages_11 or final.cudaPackagesVersions.cudaPackages_11_8;
+  cudaPackages_11 = getAttrOr prev "cudaPackages_11" final.cudaPackagesVersions.cudaPackages_11_8;
 }
