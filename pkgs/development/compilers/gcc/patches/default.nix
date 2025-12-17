90778fd7baaefea2bf79b19b50a12b3e3021e56b
Sergei Trofimovich <slyich@gmail.com>
2025-12-17 06:27:45 +0000

    gcc: don't apply c++tools --enable-default-pie patch for

--- pkgs/development/compilers/gcc/patches/default.nix
+++ pkgs/development/compilers/gcc/patches/default.nix
@@ -125,7 +125,7 @@ in
 ++ optional (atLeast14 && !canApplyIainsDarwinPatches) ./cfi_startproc-reorder-label-14-1.diff
 # c++tools: Don't check --enable-default-pie.
 # --enable-default-pie breaks bootstrap gcc otherwise, because libiberty.a is not found
-++ optional atLeast14 ./c++tools-dont-check-enable-default-pie.patch
+++ optional (is14 || is15) ./c++tools-dont-check-enable-default-pie.patch
 
 ## 2. Patches relevant to gcc>=12 on specific platforms ####################################
 
