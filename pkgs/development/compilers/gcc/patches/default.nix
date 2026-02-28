c891dc6c70a800a77149e978a87e68714eb18028
Aleksi Hannula <ahannula4+nixgit@gmail.com>
2026-02-28 14:40:05 +0200

    pkgsi686Linux.pkgsMusl.gcc: Use libssp_nonshared from musl
    This may not have visible effects until #494106 is merged. This is
    because this build is blocked by the exact same issue in
    minimal-bootstrap gcc, which causes perl to fail on this specific
    platform.

--- pkgs/development/compilers/gcc/patches/default.nix
+++ pkgs/development/compilers/gcc/patches/default.nix
@@ -319,6 +319,7 @@ in
     hash = "sha256-8I2G4430gkYoWgUued4unqhk8ZCajHf1dcivAeuLZ0E=";
   })
 ]
+++ optional (targetPlatform.isMusl && targetPlatform.isx86_32) ./libssp-noshared-musl32.patch
 
 ## Windows
 
